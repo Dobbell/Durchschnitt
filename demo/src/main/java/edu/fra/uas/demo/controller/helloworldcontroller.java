ba7590316a68d3960ffe93b2739e788ca1617ec0
Lenny <111445712+Dobbell@users.noreply.github.com>
2023-11-07 16:35:41 +0000

    Mittelwert in Web

--- demo/src/main/java/edu/fra/uas/demo/controller/helloworldcontroller.java
+++ demo/src/main/java/edu/fra/uas/demo/controller/helloworldcontroller.java
@@ -13,45 +13,71 @@ import edu.fra.uas.demo.DemoApplication;
 @Controller
 public class helloworldcontroller {
     
-    @RequestMapping(value="hello", method=RequestMethod.GET)
-    @ResponseBody
-    public String getHelloWorld() {
-        return "<h1>Hello World Spring Boot!</h1>";
-    }
+    Fach geo = new Fach(2);
+    Fach Mathe = new Fach(3); 
+    Fach Deutsch = new Fach(2);
+    Fach Englisch = new Fach(3);
+    Fach Sport = new Fach(1);
+
+    Fach[] fächer = {Mathe, Englisch, Deutsch, Sport, geo};
+    double Durchschnitt = berechneDurchschnitt(fächer);
+
+    public static double berechneDurchschnitt(Fach[] fächer) {
+        int summe = 0;
+        int counter =0;
 
 
+        for (int i =0; i < fächer.length; i++) {
+               summe += fächer[i].getnote();
+               counter ++;              
+           }
 
-
-    public static void main(String[] args) {
-		
-        Fach Mathe = new Fach(3); 
-        Fach Deutsch = new Fach(2);
-        Fach Englisch = new Fach(3);
-        Fach Sport = new Fach(1);
-
-        Fach[] fächer = {Mathe, Englisch, Deutsch, Sport} ;
-
-        double Durchschnitt = berechneDurchschnitt(fächer);
-        System.out.println("Durchschnitt: " + Durchschnitt);
-
-
-    }
-        public static double berechneDurchschnitt(Fach[] fächer) {
-            int summe = 0;
-            int counter = 0;
-
-            for (int i =0; i < fächer.length; i++) {
-                summe += fächer[i].getnote();
-                counter ++;
-                
-            }
-
-            double mittelwert = (double)summe / counter;
-            return mittelwert;
+           double mittelwert = (double)summe / counter;
+           return mittelwert;
+           
 
         }
 
-	}
+    @RequestMapping(value="hello", method=RequestMethod.GET)
+    @ResponseBody
+    public double getHelloWorld() {
+        return berechneDurchschnitt(fächer);
+        //return "<h1>Hello World Spring Boot!</h1>";
+    }
+
+}
+
+
+   // public static void main(String[] args) {
+	//	
+    //    Fach Mathe = new Fach(3); 
+    //    Fach Deutsch = new Fach(2);
+     //   Fach Englisch = new Fach(3);
+     //   Fach Sport = new Fach(1);
+
+     //   Fach[] fächer = {Mathe, Englisch, Deutsch, Sport} ;
+
+     //   double Durchschnitt = berechneDurchschnitt(fächer);
+    //    System.out.println("Durchschnitt: " + Durchschnitt);
+
+
+   // }
+   //     public static double berechneDurchschnitt(Fach[] fächer) {
+   //         int summe = 0;
+   //         int counter = 0;
+
+    //        for (int i =0; i < fächer.length; i++) {
+   //             summe += fächer[i].getnote();
+   //             counter ++;
+                
+  //          }
+
+  //          double mittelwert = (double)summe / counter;
+  //          return mittelwert;
+
+  //      }
+
+//	}
 
 
 
